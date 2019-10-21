9fd319af9743797acc3ae4e9b898273c021be158
alinoleumm <albina.li@nu.edu.kz>
2019-10-21 23:42:04 +0600

    added fields and getters to Token

--- src/splat/lexer/Lexer.java
+++ src/splat/lexer/Lexer.java
@@ -1,17 +1,38 @@
 package splat.lexer;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Lexer {
 
+	private File progFile;
+	private List<Token> tokens;
+
 	public Lexer(File progFile) {
-		// TODO Auto-generated constructor stub
+		this.progFile = progFile;
+		this.tokens = new ArrayList<>();
 	}
 
 	public List<Token> tokenize() throws LexException {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			FileReader fr = new FileReader(progFile);
+			BufferedReader br = new BufferedReader(fr);
+			int i;
+			String token = "";
+//			int line = 0;
+//			int column = 0;
+//			int startColumn = 0;
+			while ((i = br.read()) != -1) {
+				System.out.println((char) Character.toLowerCase(i));
+			}
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
+		return tokens;
 	}
 
 }
--- src/splat/lexer/Token.java
+++ src/splat/lexer/Token.java
@@ -2,4 +2,29 @@ package splat.lexer;
 
 public class Token {
 
+    private String value;
+    private int line;
+    private int column;
+
+    public Token(String value, int line, int column) {
+        this.value = value;
+        this.line = line;
+        this.column = column;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public int getLine() {
+        return line;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    public String toString() {
+        return value + " is at line " + line + " and column " + column;
+    }
 }
